4f2caeb52f27e1e49bd2f16583a8034ae5f284d0
Javi <javier.t.sartore@gmail.com>
2022-07-08 01:17:27 -0300

    resolucion del ejercicio

--- Clase 2 Git/index.js
+++ Clase 2 Git/index.js
@@ -44,29 +44,63 @@ const proceso = {
     getCodigo : function(){
         return this.codigo
     },
-
+//Habria que recorrer esta lista y devolver cual es el minimo y devolver esa lista.
     stockActual: [{codigoProducto: 1, cantidad: 10, stockMinimo: 5}, 
                   {codigoProducto: 2, cantidad: 8, stockMinimo: 12},
                   {codigoProducto: 3, cantidad: 8, stockMinimo: 13}
 
     ],
-    stockNuevo: [],
+    //el prod 3 pasar a tener 11 y el 2 a 18
+    //Hay q actualizar el stock actual 
+    stockNuevo: [{codigoProducto: 3, paraAgregar: 3},
+                 {codigoProducto: 2, paraAgregar: 10}],
 
                         //js es un lenguaje de 1er orden, porue a una funcion anonima en est caso la podemos asignar a una variable.
                         //Esta funcion desde afuera la llamariamos proceso.calcularMinimos();
+
+                        //recorrer stockActual y devolver los productos con stock minimo
+        
     calcularMinimos: function (){
 
-                        //Para evitar un cambio del estilo let proceso = 5, se usa const
+        //Para evitar un cambio del estilo let proceso = 5, se usa const
         const productosStockMinimo = [];
+        let minimo = 10000000;
+        //Recorremos el stockActual buscando el minimo
+        for (let index = 0; index < this.stockActual.length; index++) {
+            if (this.stockActual[index].stockMinimo <= minimo) {
+                minimo = this.stockActual[index];
+            }                       
+        }
+        productosStockMinimo.push(minimo)
+                     
         return productosStockMinimo
     },
                         //Si dejamos una funcion que no tiene nada devolveria undefined
-    calcular: function(){
+    actualizarStock: function(){
+        for (let index = 0; index < this.stockNuevo.length; index++) {
+            let codigoBuscadoStockNvo = this.stockNuevo[index].codigoProducto;
+            
+            for (let x = 0; x < this.stockActual.length; x++) {
+                
+                let codBuscado = this.stockActual[x].codigoProducto
 
+                if (codigoBuscadoStockNvo == codBuscado){
+                    this.stockActual[x].cantidad = this.stockActual[x].cantidad + this.stockNuevo[index].paraAgregar
+                }
+                
+            }
+        }
     }
 }
 
-console.log(proceso);
-console.log(proceso.calcular());
+console.log(proceso.stockActual);
+
+console.log(proceso.calcularMinimos());
+
+proceso.actualizarStock();
+
+console.log("Mostramos actualizado");
+console.log(proceso.stockActual);
+
 
 
