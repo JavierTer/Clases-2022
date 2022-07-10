d08525dc66d0397d3f13d37bab47cd9218e0bbf5
Javi <javier.t.sartore@gmail.com>
2022-07-10 18:49:13 -0300

    clase 5

---
+++ Clase 5/ejercicio1/src/components/Sistema.vue
@@ -0,0 +1,20 @@
+<template>
+<Cabecera/>
+  <h1>Sistema/</h1>
+</template>
+
+<script>
+import CabeceraVue from "./Cabecera.vue";
+
+export default {
+    components: {
+        Cabecera
+    }
+}
+</script>
+
+<style>
+h1 {
+  color:red;
+}
+</style>
