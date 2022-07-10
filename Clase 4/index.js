efd461a428cc18f61ecb2cdc42493d31f9715d68
Javi <javier.t.sartore@gmail.com>
2022-07-10 04:08:22 -0300

    agrego methods y nvs directivas

--- Clase 4/index.js
+++ Clase 4/index.js
@@ -1,13 +1,29 @@
+
 const objeto = Vue.createApp ( {
     data() {
         return {
             mensaje: 'Hola Vue!',
             color: {color:"red", background:"black"},
             numero: 0,
-            ok: true
+            ok: false, 
+            ver: true
         }
      },
      mounted() {                    // esto imprime cada vez que se actualiza la pagina
         console.log('componente creado');
+    }, 
+    methods: {
+        incrementar() {
+            this.numero++  //con this hacemos referencia al data
+        },
+        mostrar(){
+            if(this.ver == true){
+                this.ver = false;
+            }
+            else {
+                this.ver = true;
+            }
+             
+        }
     }
 }).mount('#app')
