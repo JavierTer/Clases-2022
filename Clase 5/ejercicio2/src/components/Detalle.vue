2c61ca38f824598a164daacb6fe29fc11fc379bb
Javi <javier.t.sartore@gmail.com>
2022-07-11 00:51:30 -0300

    ejercicio nuevo desde cero

---
+++ Clase 5/ejercicio2/src/components/Detalle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click='aviso'>Aviso</button>
+  </div>
+</template>
+
+<script>
+export default {
+emits: ['avisoPadre'],
+methods: {
+    aviso(){
+        console.log("Avisar");
+        this.$emit('avisoPadre', {propiedad: "Avisar hora de fin de clase"})
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ Clase 5/ejercicio2/src/components/Main.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <Cabecera :titulo='titulo'/>
+        <h2>Main</h2>
+        <Detalle @avisoPadre="avisar($event)"/>
+        <h4>{{mensaje}}</h4>
+    </div>
+</template>
+
+<script>
+import Cabecera from './Cabecera.vue'
+import Detalle from './Detalle.vue'
+export default {
+    components: {
+        Cabecera, Detalle
+    },
+    data() {
+        return {
+            titulo: 'ejercicio sistema...',
+            mensaje: ''
+        }
+    },
+    methods: {
+        avisar(obj) {
+            console.log('aviso padre ' + obj.propiedad);
+            this.mensaje = obj.propiedad;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
