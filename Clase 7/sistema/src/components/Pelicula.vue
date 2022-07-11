79c6f366e077489607bb8dd9f39533fc7c592880
Javi <javier.t.sartore@gmail.com>
2022-07-11 02:27:36 -0300

    vue router

---
+++ Clase 7/sistema/src/components/Pelicula.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    <h2>
+        Pelicula
+    </h2>
+    
+   <!-- Código de pelicula <input type="text"> -->
+    
+
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ Clase 7/sistema/src/components/Reserva.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>
+         Reservas
+    </h2>
+   <!-- Codigo de reserva<input type="text"> -->
+    </div>
+    
+    
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
